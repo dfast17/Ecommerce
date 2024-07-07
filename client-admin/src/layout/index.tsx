4f978db5ef93e547e659977e32e0a0b3f8991216
Nguyễn Tấn Hiếu <tanhieu1998it@gmail.com>
2024-07-07 13:21:06 +0700

    chore: update admin layout

--- client-admin/src/layout/index.tsx
+++ client-admin/src/layout/index.tsx
@@ -1,23 +1,18 @@
 /* import { useContext } from "react" */
 /* import LoadingComponent from "../loading/loadingComponent" */
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 /* import { StateContext } from "../../context/stateContext" */
-import Header from "./header"
-import Auth from "../pages/auth"
-
+import Auth from "../pages/auth";
+import AdminLayout from "./AdminLayout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const location = useLocation()
+  const location = useLocation();
   /* const {isLoading} = useContext(StateContext) */
-  return location.pathname !== "/auth" ?
-    <section className="max-w-full w-full !h-screen flex flex-wrap content-start justify-center thumbnails-color z-40">
-      {/* {isLoading && <LoadingComponent />} */}
-      <Header />
-      <main className="w-full h-auto min-h-[99vh] overflow-y-auto thumbnails-color">
-        <div className="w-full h-auto">{children}</div>
-      </main>
-    </section>
-    : <Auth />
-}
+  return location.pathname !== "/auth" ? (
+    <AdminLayout>{children}</AdminLayout>
+  ) : (
+    <Auth />
+  );
+};
 
-export default Layout
+export default Layout;
